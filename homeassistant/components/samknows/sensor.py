dc220f07f6a8a7275ade8f102543608ca84dc7a6
Paul Lessing <paul@paullessing.com>
2023-08-16 20:50:24 +0000

    feat: Set up entity on load

--- homeassistant/components/samknows/sensor.py
+++ homeassistant/components/samknows/sensor.py
@@ -1,8 +1,21 @@
+from datetime import timedelta
+import logging
+
+import async_timeout
+
+from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 from .const import DOMAIN
+from .whitebox import WhiteboxApi
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -10,9 +23,107 @@ async def async_setup_entry(
 ) -> None:
     """Set up the sensor platform."""
 
-    hass.data[DOMAIN][entry.entry_id]
-    entities = []
+    api = hass.data[DOMAIN][entry.entry_id]
+    # entities = []
+
+    # assuming API object stored here by __init__.py
 
     # entities.append(SensorEntity())
+    coordinator = WhitebooxCoordinator(hass, api)
 
-    async_add_entities(entities)
+    # Fetch initial data so we have data when entities subscribe
+    #
+    # If the refresh fails, async_config_entry_first_refresh will
+    # raise ConfigEntryNotReady and setup will try again later
+    #
+    # If you do not want to retry setup on failure, use
+    # coordinator.async_refresh() instead
+    #
+    await coordinator.async_config_entry_first_refresh()
+
+    _LOGGER.info("Coordinator has run %s", coordinator.data)
+
+    async_add_entities(
+        WhiteboxEntity(coordinator, unit_id) for unit_id in coordinator.data
+    )
+
+
+class WhitebooxCoordinator(DataUpdateCoordinator):
+    """Whitebox coordinator."""
+
+    def __init__(self, hass: HomeAssistant, api: WhiteboxApi) -> None:
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="Whitebox",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=300),
+        )
+        self.api = api
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+                # Grab active context variables to limit data required to be fetched from API
+                # Note: using context is not required if there is no need or ability to limit
+                # data retrieved from API.
+                listening_idx = set(self.async_contexts())
+                _LOGGER.warning("Listening idx %s", listening_idx)
+                data = await self.api.fetch_data()  # (listening_idx)
+                _LOGGER.info("Fetched data %s", data)
+                return data
+        # except ApiAuthError as err:
+        #     # Raising ConfigEntryAuthFailed will cancel future updates
+        #     # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+        #     raise ConfigEntryAuthFailed from err
+        # except ApiError as err:
+        #     raise UpdateFailed(f"Error communicating with API: {err}")
+        finally:
+            _LOGGER.warning("Done updating")
+
+
+class WhiteboxEntity(CoordinatorEntity[WhitebooxCoordinator], SensorEntity):
+    """An entity using CoordinatorEntity.
+
+    The CoordinatorEntity class provides:
+      should_poll
+      async_update
+      async_added_to_hass
+      available
+
+    """
+
+    def __init__(self, coordinator: WhitebooxCoordinator, unit_id: str) -> None:
+        """Pass coordinator to CoordinatorEntity."""
+        _LOGGER.debug("Initialising entity %s", unit_id)
+        super().__init__(coordinator, context=unit_id)
+        self.unit_id = unit_id
+
+        self._attr_device_class = SensorDeviceClass.DATA_RATE
+        self._attr_unique_id = f"whitebox_{unit_id}"
+        self._attr_native_unit_of_measurement = "B/s"
+        self._attr_suggested_unit_of_measurement = "Mbit/s"
+        self._attr_name = f"Whitebox {unit_id}"
+
+        self._attr_native_value = self.coordinator.data[self.unit_id]["results"][0][
+            "metricValue"
+        ]
+
+        _LOGGER.debug("Data is %s", self.coordinator.data[self.unit_id])
+        _LOGGER.debug("Value %s", self._attr_state)
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        _LOGGER.debug("Coordinator update %s", self.unit_id)
+        self._attr_state = True  # self.coordinator.data[self.idx]["state"]
+        self.async_write_ha_state()
--- homeassistant/components/samknows/whitebox.py
+++ homeassistant/components/samknows/whitebox.py
@@ -62,3 +62,54 @@ class WhiteboxApi:
         )
         _LOGGER.warning("Decoded JWT, %s", decoded_data)
         return datetime.fromtimestamp(decoded_data["exp"])
+
+    async def _get_fresh_token(self) -> str:
+        if datetime.now() >= self.access_token_expiry:
+            refreshed = await self._refresh_token()
+            if not refreshed:
+                logged_in = await self.login(self._session)
+                if not logged_in:
+                    raise CouldNotAuthenticate()
+        return self.access_token
+
+    async def _refresh_token(self) -> bool:
+        if datetime.now() >= self.refresh_token_expiry:
+            return False
+
+        async with self._session.post(
+            "https://sentinel-api.cloud.samknows.com/renew",
+            headers={"Content-Type": "application/json"},
+            json={"refreshToken": self.refresh_token},
+        ) as resp:
+            try:
+                response = await resp.json()
+                _LOGGER.info(response)
+                if response.get("code") == "OK":
+                    new_access_token = response["data"]["accessToken"]
+                    self.access_token = new_access_token
+                    self.access_token_expiry = self._get_token_expiry(
+                        self, new_access_token
+                    )
+                    return True
+                return False
+            except Exception as e:
+                _LOGGER.error("Failed to renew access token, %s", e)
+                return False
+
+    async def fetch_data(self):
+        token = await self._get_fresh_token()
+        async with self._session.get(
+            "https://unit-analytics-api.cloud.samknows.com/102479809/scheduled_tests_daily?date=2023-08-16&metric=httpgetmt",
+            headers={"Authorization": f"Bearer {token}"},
+        ) as resp:
+            response = await resp.json()
+            if response.get("code") == "OK":
+                return {"102479809": response["data"]}
+
+
+class RefreshFailed(Exception):
+    """Raised when refreshing an auth token has failed."""
+
+
+class CouldNotAuthenticate(Exception):
+    """Raised when authentication has failed."""
